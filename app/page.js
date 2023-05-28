83d16e08eeef4877c2ab0b876222bc338e233933
jaime <garciadiazjaime@gmail.com>
2023-05-28 15:52:42 -0500

    feat: remove fetch

--- app/page.js
+++ app/page.js
@@ -1,17 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-
-import runETL from "../support/etl";
-
 export default function Home() {
-  const init = async () => {
-    await runETL();
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
-
   return <main>😊</main>;
 }
